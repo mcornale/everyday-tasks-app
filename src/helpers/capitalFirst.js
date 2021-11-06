e061e4cd64053278042b2c9d8fe2b815fe7685c0
mcornale <massimiliano.cornale@gmail.com>
2021-11-06 20:44:04 +0100

    Fix capitalFirst function

--- src/helpers/capitalFirst.js
+++ src/helpers/capitalFirst.js
@@ -1,6 +1,6 @@
 const capitalFirst = (string) => {
   const transformedString = string.trim().toLowerCase();
-  return transformedString[0].toUpperCase() + transformedString.slice(1, -1);
+  return transformedString[0].toUpperCase() + transformedString.slice(1);
 };
 
 export default capitalFirst;
